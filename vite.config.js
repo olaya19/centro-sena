450f0cd8927364b3a9b1fe33c0b7518635a50db2
olaya19 <geraldinolaya88@gmail.com>
2025-10-30 10:12:35 -0500

    deploy:trigger Github Pages biuld Centro Sena

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/centro-sena/', // 👈 importante: debe tener slash inicial y final
 })
